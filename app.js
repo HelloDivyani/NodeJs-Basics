5e3d20c46ce69e18557106dd8e8488ed82be1a82
Divyani Mittal <hellodivyani@gmail.com>
2017-05-24 18:22:55 +0530

    Events and util core modules added

--- app.js
+++ app.js
@@ -34,4 +34,44 @@ console.log("The stuff : "+stuff.myAdder(3,4));
 
 console.log('In app There are '+stuff.myCounter(['123','213']));
 console.log('In addition  '+stuff.myAdder(stuff.myPi,stuff.myPi));
+// Using Core Event event
+var events = require("events"); // Events is the core event
+var util  = require("util");
+
+
+// Create an Object Constructor : 
+var Person = function(name)
+{
+	//this.name is the global name variable
+	this.name = name;
+}
+util.inherits(Person,events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+
+var people = [james,mary,ryu];
+
+// Added Person
+
+people.forEach(function(person){
+	person.on('speak',function(msg){
+			console.log(person.name+" said : "+msg);
+		
+	});
+	
+});
+
+james.emit("speak",'hello');
+ryu.emit("speak",'hello james');
+
+var myEmitter = new events.EventEmitter();
+//myCustom Emitter from events module 
+
+myEmitter.on('someEvent',function(msg){
+console.log(msg);
+});
+
+myEmitter.emit('someEvent','My Message');
 
